73037d5885a85a0476d66600d81fe03b69c00237
Alberto Murillo <albertomurillosilva@gmail.com>
2024-02-05 16:25:19 -0800

    lib/log: annoate log_fatal as noreturn

--- src/lib/log/log.h
+++ src/lib/log/log.h
@@ -1,7 +1,9 @@
 #ifndef LOG_H
 #define LOG_H
 
-void log_fatal(const char *msg);
-void log_fatalf(const char *format, ...);
+#include <stdnoreturn.h>
+
+noreturn void log_fatal(const char *msg);
+noreturn void log_fatalf(const char *format, ...);
 
 #endif // LOG_H
